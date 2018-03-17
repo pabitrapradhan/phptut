f5948b531705ed5a478ca4ac1f5f9e6b861c7743
Smith <sourav.cantrip@gmail.com>
2018-03-17 15:14:45 +0530

    No need to use php ending tag unnecessarily

---
+++ string/string_functions.php
@@ -0,0 +1,16 @@
+<?php
+echo "Welcome";
+?>
+<html>
+	<head>
+		<title>String Functions</title>
+	</head>
+	<body>
+		<?php
+		echo "<table>";
+		echo "</table>";
+		?>
+	</body>
+</html>
+<?php
+echo "World";
