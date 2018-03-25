bd797d011fe38f0c39c4d0d568359d75ca9768bb
Smith <sourav.cantrip@gmail.com>
2018-03-25 13:39:08 +0530

    Edit script

--- string/add_student.php
+++ string/add_student.php
@@ -1,4 +1,25 @@
 <?php
 //print_r($_POST);
-$stdname =  $_POST['stdname'];
-$stdroll =  $_POST['stdroll'];
+$con=mysqli_connect("localhost","root","root",'pabitra') or die('error');
+
+//Array ( [student] => s [roll] => 1 ) 
+
+$stdname =  $_POST['student'];
+$stdroll =  $_POST['roll'];
+//$spl1="INSERT INTO project ('name', 'roll') VALUES (".$stdname.",".$stdroll.")";
+
+//echo $spl1; exit();
+$query = "INSERT INTO `project` (`name`, `roll`) VALUES ('".$stdname."','".$stdroll."')";
+//echo $query;
+$sql=mysqli_query($con, $query);
+
+
+if($sql==true)
+{
+	echo "true";
+}
+else
+{
+	echo "false";
+}
+
--- string/ajax.php
+++ string/ajax.php
@@ -1,3 +1,6 @@
+<?php 
+$con=mysqli_connect("localhost","root","root",'pabitra') or die('error');
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,8 +10,8 @@
 		$(document).ready(function(){
 			
 			$("#addStd").click(function(){
-				var stdname = $("#stdname").val();
-				var stdroll = $("#stdroll").val();
+				var stdname = $("#student").val();
+				var stdroll = $("#roll").val();
 				$.ajax({
 					url:'http://localhost/pabitra-phptut/string/add_student.php',
 					type:'POST',
@@ -16,7 +19,10 @@
 					//data : {'stdname':stdname, 'stdroll': stdroll},
 					data : $("#addStdForm").serialize(),
 					success : function(response){
-						console.log(stdname);
+						if (response) {
+							$("#success").html("Inserted successfully");
+						}
+						console.log(response);
 						$("#stdListHolder").append("<tr><td>"+stdname+"</td><td>"+stdroll+"</td></tr>");
 					}
 				});
@@ -27,15 +33,34 @@
 </head>
 <body>
 	<form id="addStdForm">
-		<input type="text" name="student" id="stdname">
-		<input type="number" name="roll" id="stdroll">
+		<input type="text" name="student" id="student">
+		<input type="number" name="roll" id="roll">
 		<input type="button" name="Submit" value="Submit" id="addStd">
 	</form>
+	<div id="success"></div>
 	<table id="stdListHolder">
 		<tr>
 			<td>Name</td>
 			<td>Roll</td>
+			<td>Edit</td>
 		</tr>
+		<?php
+$query = "select * from project";
+$data = mysqli_query($con, $query);
+//print_r($data);
+while($data1=mysqli_fetch_array($data))
+//echo $data1['name'];
+{
+
+?>
+		<tr>
+		<td><input type="text" name="student1" id="student1" value="<?php echo $data1['name'];?>"></td>
+		<td><input type="text" name="roll1" id="roll1" value="<?php echo $data1['roll'];?>"></td>
+		<td><a href="update.php?Id=<?php echo $data1['id'];?>">Update</a></td>	
+		</tr>
+		<?php
+			}
+		?>
 	</table>
 </body>
 </html>
